57368965517927457c5c12b5eb83c9c6e3d1ea34
Peng Hanlin <i@lenconda.top>
2021-03-02 14:28:18 +0800

    Update .dollie.js

--- .dollie.js
+++ .dollie.js
@@ -3,7 +3,9 @@ module.exports = {
   files: {
     add: [
       (context) => {
-        if (context.findIndex((item) => item.scaffoldName.indexOf('extend-scaffold-react-ts') !== -1) !== -1) {
+        console.log(context);
+        const index = context.findIndex((item) => item.scaffoldName.indexOf('extend-scaffold-react-ts') !== -1);
+        if (index !== -1) {
           return ['src/models/**/*.ts', 'src/patches/**/*.tsx'];
         } else {
           return ['src/models/**/*.js', 'src/patches/**/*.js'];
